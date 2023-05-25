6e864278fd1f91e1853ff849cd16f4600c9648ab
Jason Hrabi <jason.hrabi@microfocus.com>
2023-05-25 13:34:06 -0400

    Modified with new configurations from Octane team.

---
+++ envsetup/install_k8s.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+sudo yum update -y
+sudo yum install docker -y
+sudo service docker start
+sudo yum install git -y
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+sudo yum install -y kubectl
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo usermod -aG docker $USER && newgrp docker
+minikube start
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+git clone https://github.com/jhrabi/octane-helm
